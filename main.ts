9a1f8805bd0900715a77cffd84791774879c402c
MarcOlivier91 <smarc.mailpro@gmail.com>
2024-09-18 19:04:32 +0200

    starting comparing choices

--- main.ts
+++ main.ts
@@ -2,13 +2,20 @@
 import * as readline from 'readline-sync'
 
 function rps() {
-    const choices: String[] = [ "rock", "paper", "scissors"]
+    const choices: string[] = [ "rock", "paper", "scissors"]
     var userInput: string = readline.question('Pick a choice ');
     var userChoice = userInput.toLowerCase()
     console.log('You choose : ' + userChoice)
 
     const cpuChoice = Math.floor(Math.random() * choices.length)
-    console.log("Opponent choose : " + choices[cpuChoice]) 
+    console.log("Opponent choose : " + choices[cpuChoice])
+    
+    if (userChoice === choices[cpuChoice]) {
+        console.log("Its a draw !")
+    } else {
+        console.log("Someone might have won...")
+    }
+
     return 0
 }
 

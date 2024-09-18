1edf67e81592b78c31439ea39ae2fe6ae70ecfdd
MarcOlivier91 <smarc.mailpro@gmail.com>
2024-09-18 17:19:45 +0200

    added user input

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
-// Let's build from here 
+// Let's build from here
+import * as readline from 'readline-sync'
 
 function rps() {
     let choices = [ "rock", "paper", "scissors"]
     console.log(choices)
+    var userInput = readline.question('Pick a choice ');
+    console.log('You choose : ' + userInput)
     return 0
 }
 

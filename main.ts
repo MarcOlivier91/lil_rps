3688cdb9e948ada04a7308a6a71f267c7c5a662c
MarcOlivier91 <smarc.mailpro@gmail.com>
2024-09-20 15:55:59 +0200

    changed from typing to picking a choice

--- main.ts
+++ main.ts
@@ -3,18 +3,17 @@ import * as readline from 'readline-sync'
 
 function rps() {
     const choices: string[] = [ "rock", "paper", "scissors"]
-    var userInput: string = readline.question('Pick a choice ');
-    var userChoice = userInput.toLowerCase()
-    console.log('You choose : ' + userChoice)
+    var userInput: string = readline.keyInSelect(choices, 'Pick a choice ');
+    console.log('You choose : ' + choices[userInput])
 
     const cpuChoice = Math.floor(Math.random() * 3)
     console.log("Opponent choose : " + choices[cpuChoice])
-    
-    if (userChoice === choices[cpuChoice]) {
+    console.log("******")
+
+    if (choices[userInput] === choices[cpuChoice]) {
         console.log("It's a draw !")
     } else {
-        console.log("Someone might have won...")
-        switch(userChoice) {
+        switch(choices[userInput]) {
             case "rock":
                 var result = (choices[cpuChoice] === "scissors") ? "You Win !" : "You Lose !"
                 console.log(result)
